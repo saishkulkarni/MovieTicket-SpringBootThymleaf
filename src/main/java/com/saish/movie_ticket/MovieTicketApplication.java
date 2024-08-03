d180255bd1e64c8364677d8aecc3843fbef13a44
saishkulkarni <saishkulkarni7@gmail.com>
2024-08-03 17:45:26 +0530

    Project Created

---
+++ src/main/java/com/saish/movie_ticket/MovieTicketApplication.java
@@ -0,0 +1,13 @@
+package com.saish.movie_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saish/movie_ticket/MovieTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saish.movie_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
