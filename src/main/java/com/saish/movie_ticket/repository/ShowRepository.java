408e0422b2c47de9f715da567107644a242e399b
saishkulkarni <saishkulkarni7@gmail.com>
2024-08-26 10:23:12 +0530

    Done

--- src/main/java/com/saish/movie_ticket/repository/ShowRepository.java
+++ src/main/java/com/saish/movie_ticket/repository/ShowRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.saish.movie_ticket.dto.Movie;
 import com.saish.movie_ticket.dto.Screen;
-import java.util.List;
-import com.saish.movie_ticket.dto.Movie;
-import com.saish.movie_ticket.dto.Screen;
 import com.saish.movie_ticket.dto.Show;
 
 public interface ShowRepository extends JpaRepository<Show, Integer> {
